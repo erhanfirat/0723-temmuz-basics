840f4e0638feb5a19d9670c0ef313700303fe50d
Erhan Fırat <erhanfirat@gmail.com>
2023-08-24 11:18:39 +0300

    async await axios

--- dom.js
+++ dom.js
@@ -57,9 +57,79 @@ function createProductCard(product) {
   return cardDiv;
 }
 
-const pContainer = document.querySelector(".products-container");
+function createProductCards(productsData) {
+  const pContainer = document.querySelector(".products-container");
 
-for (let product of productsData) {
-  const productCard = createProductCard(product);
-  pContainer.append(productCard);
+  for (let product of productsData) {
+    const productCard = createProductCard(product);
+    pContainer.append(productCard);
+  }
 }
+
+//----------------------------------
+
+// const sifirdanYuzeKadarRastgeleCiftSayiBul = () => {
+//   return new Promise((resolve, reject) => {
+//     console.log("düşünüyoruummm…");
+//     setTimeout(() => {
+//       const sayi = Math.floor(Math.random() * 100);
+//       if (sayi % 2 === 0) {
+//         resolve(sayi);
+//       } else {
+//         reject(sayi);
+//       }
+//     }, 3000);
+//   });
+// };
+
+// const sayiPromise = sifirdanYuzeKadarRastgeleCiftSayiBul();
+// // sayiPromise yeni bir promise objesidir
+
+// // Promise chaining
+
+// sayiPromise
+//   .then((response) => {
+//     console.log("sayiPromise then success response: ", response);
+//     return response + " başarılı";
+//   })
+//   .then((res) => {
+//     console.log("2. then çalıştı ", res);
+//     return "ali";
+//   })
+//   .catch((response) => {
+//     console.log("sayiPromise catch error response: ", response);
+//     throw response + " çift değil!";
+//   })
+//   .catch((response) => {
+//     console.log("2. catch: ", response);
+//     throw "Sonuç hatalı!";
+//   })
+//   .then((res) => {
+//     console.log("3. then çalıştı ", res);
+//   })
+//   .finally(() => {
+//     console.log("hatasıyla doğrusuyla işlem sonlandı!");
+//   });
+
+axios
+  .get("https://620d69fb20ac3a4eedc05e3a.mockapi.io/api/products")
+  .then((res) => res.data)
+  .then((productsData) => {
+    createProductCards(productsData);
+  })
+  .catch((err) => {
+    console.error("axios isteğinde hata oluştu", err);
+  });
+
+const getProducts = async function () {
+  try {
+    const response = await axios.get(
+      "https://620d69fb20ac3a4eedc05e3a.mockapi.io/api/products123"
+    );
+    console.log("response: ", response);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getProducts();
